32fbcdf895b4d8a140bbbe8c193d6ee97d37f9bc
Asya Tealdi <asya2698@live.it>
2022-06-28 10:57:45 +0200

    Initial Commit

---
+++ HackerNewsClient/HackerNewsClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsClientApp.swift
+//  HackerNewsClient
+//
+//  Created by Asya Tealdi on 28/06/22.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
